6e560121889eae8d0b80cf2c23bbe4b60e5c7a51
Veronika <veronikapetrushka@gmail.com>
2024-04-13 19:45:01 +0200

    store changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+ import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./rootReducer";
 
@@ -10,10 +19,16 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({
+export const store = configureStore({
   reducer: persistedReducer,
-});
+  filters: filtersReducer,
+},
+   middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+);
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export let persistor = persistStore(store);
